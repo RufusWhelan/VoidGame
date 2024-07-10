a984ba5fccf87533ad58e316de17a3e5081d1091
RufusWhelan <rufus.whelan@education.nsw.gov.au>
2024-07-10 12:30:06 +1000

    Update Player.gd

--- void_game/Player.gd
+++ void_game/Player.gd
@@ -8,13 +8,16 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var dashing: bool = false
+var gravityExists = true
 
 func _input(_event):
 	if Input.is_action_just_pressed('primary_action'): dash()
 
 func _physics_process(delta):
+	
 	if !dashing:
-		if not is_on_floor():
+		voidGravity()
+		if gravityExists == true && not is_on_floor():
 			velocity.y += gravity * delta
 
 		if Input.is_action_pressed("jump") and is_on_floor():
@@ -39,7 +42,6 @@ func _process(_delta):
 
 func dash():
 	dashing = true
-	Globals.in_void = true
 	sprite.look_at(get_global_mouse_position())
 	$DashTimer.start(0.2)
 	$Sprite/NonVoid.visible = false
@@ -53,5 +55,24 @@ func _on_dash_timer_timeout():
 	$Sprite/Void.visible = false
 	sprite.rotation = 0
 	dashing = false
-	Globals.in_void = false
 	velocity.y = clamp(velocity.y, -400, 400)
+	
+func voidGravity():
+	if Input.is_action_pressed('voidMode'):
+		gravityExists = false
+		velocity.y = 0
+		velocity.x = 0
+		var voidUp = Input.is_action_pressed("up")
+		if voidUp:
+			velocity.y = -SPEED
+		var voidDown = Input.is_action_pressed("down")
+		if voidDown:
+			velocity.y = SPEED
+		var voidLeft = Input.is_action_pressed("left")
+		if voidLeft:
+			velocity.x = -SPEED
+		var voidRight = Input.is_action_pressed("right")
+		if voidRight:
+			velocity.x = SPEED
+	else:
+		gravityExists = true
